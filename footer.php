6c11a17824a15af00bb69f98c91b4fe219e1ae91
Siraj Khan <siraj2544@gmail.com>
2025-10-04 21:04:30 +0500

    Add responsive CSS, variable definitions, and initial HTML structure
    - Created responsive.css to handle media queries and responsive design elements.
    - Added variables.css for centralized CSS variable management including colors, typography, spacing, and breakpoints.
    - Introduced swiper-bundle.min.css for Swiper functionality.
    - Initialized main.js for future JavaScript functionality.
    - Set up header.php with essential HTML structure and linked CSS files.
    - Added footer.php with a test header for layout verification.

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+ <h1>test 3</h1>
+
+
+</div>
+<!-- wrapper class -->
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TekInvent</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/variable.css">
+    <link rel="stylesheet" href="assets/css/common.css">
+    <link rel="stylesheet" href="assets/css/normalize.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+    <!-- wrapper start -->
+    <div class="wrapper">
