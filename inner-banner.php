51872273d1e63de5d723c4e4b80a635e8b5c6379
Siraj Khan <siraj2544@gmail.com>
2025-10-09 02:02:01 +0500

    Add new inner banner sections and images for blog layout

---
+++ inner-banner.php
@@ -0,0 +1,16 @@
+<section class="inner-banner">
+    <img src="assets/images/banner-line.png" class="line line-1" alt="">
+    <img src="assets/images/banner-line.png" class="line line-2" alt="">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-10 col-12">
+                <div class="content text-center">
+                    <h1>Fueling Growth through Innovation and Creativity</h1>
+                    <p>This is dummy copy. It is not meant to be read. It has been placed here solely to demonstrate the
+                        look and feel of finished, typeset text.</p>
+                    <a href="#" class="btn btn-primary">Get in Touch</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
