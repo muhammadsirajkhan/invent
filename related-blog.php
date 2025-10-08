187447cf27eb70a3e1a70e397983a2f389c872d5
Siraj Khan <siraj2544@gmail.com>
2025-10-09 02:02:06 +0500

    Add related blog section with slider and sample content

---
+++ related-blog.php
@@ -0,0 +1,142 @@
+<section class="related">
+    <div class="container">
+        <div class="row col-lg-12">
+            <div class="blog-slider-wrap">
+                <div class="swiper blog-slider">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="blog-card">
+                                <div class="icon edge edge-base">
+                                    <img src="assets/images/sample.png" alt="">
+                                </div>
+                                <div class="title">
+                                    <h2>Dummy Title of Blog</h2>
+                                    <span class="date">02 October, 2025</span>
+                                </div>
+                                <p>This is dummy copy. It is not meant to be read. This is dummy copy. It is not
+                                    meant
+                                    to be read. This is dummy copy. It is not meant to be read.</p>
+                                <div class="author">
+                                    <img src="assets/images/author.jpg" alt="">
+                                    <div class="info">
+                                        <span class="name">John Doe</span>
+                                        <span class="verified">
+                                            <svg width="7" height="6" viewBox="0 0 7 6" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path
+                                                    d="M2.37986 5.4474L0.151978 3.23285C0.0897218 3.1706 0.0585938 3.09056 0.0585938 2.99272C0.0585938 2.89489 0.0897218 2.81485 0.151978 2.75259L0.645581 2.27233C0.707837 2.20118 0.785657 2.16561 0.879041 2.16561C0.972426 2.16561 1.05469 2.20118 1.12584 2.27233L2.61999 3.76648L5.82174 0.564733C5.89289 0.493583 5.97515 0.458008 6.06854 0.458008C6.16192 0.458008 6.23974 0.493583 6.302 0.564733L6.7956 1.04499C6.85786 1.10725 6.88899 1.18729 6.88899 1.28513C6.88899 1.38296 6.85786 1.463 6.7956 1.52526L2.86012 5.4474C2.79787 5.51855 2.71782 5.55412 2.61999 5.55412C2.52216 5.55412 2.44212 5.51855 2.37986 5.4474Z"
+                                                    fill="#A866EC" />
+                                            </svg>
+                                            Verified writer</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="blog-card">
+                                <div class="icon edge edge-base">
+                                    <img src="assets/images/sample.png" alt="">
+                                </div>
+                                <div class="title">
+                                    <h2>Dummy Title of Blog</h2>
+                                    <span class="date">02 October, 2025</span>
+                                </div>
+                                <p>This is dummy copy. It is not meant to be read. This is dummy copy. It is not
+                                    meant
+                                    to be read. This is dummy copy. It is not meant to be read.</p>
+                                <div class="author">
+                                    <img src="assets/images/author.jpg" alt="">
+                                    <div class="info">
+                                        <span class="name">John Doe</span>
+                                        <span class="verified">
+                                            <svg width="7" height="6" viewBox="0 0 7 6" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path
+                                                    d="M2.37986 5.4474L0.151978 3.23285C0.0897218 3.1706 0.0585938 3.09056 0.0585938 2.99272C0.0585938 2.89489 0.0897218 2.81485 0.151978 2.75259L0.645581 2.27233C0.707837 2.20118 0.785657 2.16561 0.879041 2.16561C0.972426 2.16561 1.05469 2.20118 1.12584 2.27233L2.61999 3.76648L5.82174 0.564733C5.89289 0.493583 5.97515 0.458008 6.06854 0.458008C6.16192 0.458008 6.23974 0.493583 6.302 0.564733L6.7956 1.04499C6.85786 1.10725 6.88899 1.18729 6.88899 1.28513C6.88899 1.38296 6.85786 1.463 6.7956 1.52526L2.86012 5.4474C2.79787 5.51855 2.71782 5.55412 2.61999 5.55412C2.52216 5.55412 2.44212 5.51855 2.37986 5.4474Z"
+                                                    fill="#A866EC" />
+                                            </svg>
+                                            Verified writer</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="blog-card">
+                                <div class="icon edge edge-base">
+                                    <img src="assets/images/sample.png" alt="">
+                                </div>
+                                <div class="title">
+                                    <h2>Dummy Title of Blog</h2>
+                                    <span class="date">02 October, 2025</span>
+                                </div>
+                                <p>This is dummy copy. It is not meant to be read. This is dummy copy. It is not
+                                    meant
+                                    to be read. This is dummy copy. It is not meant to be read.</p>
+                                <div class="author">
+                                    <img src="assets/images/author.jpg" alt="">
+                                    <div class="info">
+                                        <span class="name">John Doe</span>
+                                        <span class="verified">
+                                            <svg width="7" height="6" viewBox="0 0 7 6" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path
+                                                    d="M2.37986 5.4474L0.151978 3.23285C0.0897218 3.1706 0.0585938 3.09056 0.0585938 2.99272C0.0585938 2.89489 0.0897218 2.81485 0.151978 2.75259L0.645581 2.27233C0.707837 2.20118 0.785657 2.16561 0.879041 2.16561C0.972426 2.16561 1.05469 2.20118 1.12584 2.27233L2.61999 3.76648L5.82174 0.564733C5.89289 0.493583 5.97515 0.458008 6.06854 0.458008C6.16192 0.458008 6.23974 0.493583 6.302 0.564733L6.7956 1.04499C6.85786 1.10725 6.88899 1.18729 6.88899 1.28513C6.88899 1.38296 6.85786 1.463 6.7956 1.52526L2.86012 5.4474C2.79787 5.51855 2.71782 5.55412 2.61999 5.55412C2.52216 5.55412 2.44212 5.51855 2.37986 5.4474Z"
+                                                    fill="#A866EC" />
+                                            </svg>
+                                            Verified writer</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="blog-card">
+                                <div class="icon edge edge-base">
+                                    <img src="assets/images/sample.png" alt="">
+                                </div>
+                                <div class="title">
+                                    <h2>Dummy Title of Blog</h2>
+                                    <span class="date">02 October, 2025</span>
+                                </div>
+                                <p>This is dummy copy. It is not meant to be read. This is dummy copy. It is not
+                                    meant
+                                    to be read. This is dummy copy. It is not meant to be read.</p>
+                                <div class="author">
+                                    <img src="assets/images/author.jpg" alt="">
+                                    <div class="info">
+                                        <span class="name">John Doe</span>
+                                        <span class="verified">
+                                            <svg width="7" height="6" viewBox="0 0 7 6" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path
+                                                    d="M2.37986 5.4474L0.151978 3.23285C0.0897218 3.1706 0.0585938 3.09056 0.0585938 2.99272C0.0585938 2.89489 0.0897218 2.81485 0.151978 2.75259L0.645581 2.27233C0.707837 2.20118 0.785657 2.16561 0.879041 2.16561C0.972426 2.16561 1.05469 2.20118 1.12584 2.27233L2.61999 3.76648L5.82174 0.564733C5.89289 0.493583 5.97515 0.458008 6.06854 0.458008C6.16192 0.458008 6.23974 0.493583 6.302 0.564733L6.7956 1.04499C6.85786 1.10725 6.88899 1.18729 6.88899 1.28513C6.88899 1.38296 6.85786 1.463 6.7956 1.52526L2.86012 5.4474C2.79787 5.51855 2.71782 5.55412 2.61999 5.55412C2.52216 5.55412 2.44212 5.51855 2.37986 5.4474Z"
+                                                    fill="#A866EC" />
+                                            </svg>
+                                            Verified writer</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="blog-button-next">
+
+                    <svg width="22" height="18" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M2.17522 10.3456H17.2541L12.6389 15.8889C12.5321 16.0175 12.4516 16.1658 12.402 16.3255C12.3525 16.4852 12.3349 16.653 12.3503 16.8195C12.3813 17.1557 12.5446 17.4658 12.8042 17.6816C13.0638 17.8974 13.3986 18.0013 13.7348 17.9703C14.071 17.9393 14.3811 17.776 14.5969 17.5163L20.9539 9.88788C20.9967 9.82721 21.0349 9.76346 21.0684 9.69717C21.0684 9.6336 21.1319 9.59546 21.1574 9.53189C21.215 9.38611 21.2452 9.23093 21.2464 9.07418C21.2452 8.91743 21.215 8.76225 21.1574 8.61648C21.1574 8.55291 21.0938 8.51476 21.0684 8.45119C21.0349 8.3849 20.9967 8.32116 20.9539 8.26048L14.5969 0.632026C14.4773 0.488506 14.3276 0.373087 14.1584 0.293979C13.9892 0.214871 13.8047 0.174015 13.6179 0.174319C13.3208 0.173738 13.0329 0.2772 12.8042 0.466743C12.6755 0.573477 12.569 0.704559 12.491 0.852484C12.413 1.00041 12.365 1.16227 12.3496 1.32879C12.3343 1.49532 12.3519 1.66324 12.4016 1.82293C12.4512 1.98262 12.5319 2.13096 12.6389 2.25943L17.2541 7.80277H2.17522C1.83802 7.80277 1.51463 7.93673 1.2762 8.17516C1.03776 8.4136 0.903809 8.73698 0.903809 9.07418C0.903809 9.41138 1.03776 9.73477 1.2762 9.9732C1.51463 10.2116 1.83802 10.3456 2.17522 10.3456Z"
+                            fill="#A866EC" />
+                    </svg>
+
+                </div>
+                <div class="blog-button-prev">
+
+                    <svg width="22" height="18" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M19.8245 10.3456H4.74563L9.36084 15.8889C9.4677 16.0175 9.54819 16.1658 9.59771 16.3255C9.64724 16.4852 9.66483 16.653 9.64948 16.8195C9.61848 17.1557 9.4552 17.4658 9.19556 17.6816C8.93592 17.8974 8.60118 18.0013 8.26499 17.9703C7.92879 17.9393 7.61868 17.776 7.40287 17.5163L1.04583 9.88788C1.00306 9.82721 0.964813 9.76346 0.9314 9.69717C0.9314 9.6336 0.86783 9.59546 0.842403 9.53189C0.784775 9.38611 0.754601 9.23093 0.753405 9.07418C0.754601 8.91743 0.784775 8.76225 0.842403 8.61648C0.842403 8.55291 0.905973 8.51476 0.9314 8.45119C0.964813 8.3849 1.00306 8.32116 1.04583 8.26048L7.40287 0.632026C7.52241 0.488506 7.67211 0.373087 7.84131 0.293979C8.01052 0.214871 8.19508 0.174015 8.38186 0.174319C8.67893 0.173738 8.96682 0.2772 9.19556 0.466743C9.3243 0.573477 9.43072 0.704559 9.50872 0.852484C9.58673 1.00041 9.63478 1.16227 9.65014 1.32879C9.66549 1.49532 9.64784 1.66324 9.5982 1.82293C9.54856 1.98262 9.4679 2.13096 9.36084 2.25943L4.74563 7.80277H19.8245C20.1617 7.80277 20.4851 7.93673 20.7236 8.17516C20.962 8.4136 21.0959 8.73698 21.0959 9.07418C21.0959 9.41138 20.962 9.73477 20.7236 9.9732C20.4851 10.2116 20.1617 10.3456 19.8245 10.3456Z"
+                            fill="#A866EC" />
+                    </svg>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
